3e40a54e52e63b74175304e2a8c8f31435de74e5
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-01-05 11:20:51 +0330

    update

--- app.js
+++ app.js
@@ -53,7 +53,70 @@ appelStore.showModels(Mini)
 const { time_job } = appelStore;
 const {time_job :{sat:{open:opn,close:cls}} } = appelStore;
 console.log(opn, cls);
-appelStore.midens({
-    countery: 'USA',
+// appelStore.midens({
+//     countery: 'USA',
      
-})
+// })
+
+
+////////////// ...spread Operator
+
+const mr = [1, 2];
+const mu = [...mr, 3, 4];
+console.log(mu);
+const yt = [...mu, 5, 6, 7,...mr,...mu];
+console.log(yt);
+const rr = [...ProMax.color,'USA']
+console.log(rr);
+const er = [...ProMax.color, ...Pro.color]
+er[5]= "b";
+console.log(er);
+console.log(Pro.color);
+const gh = 'manoochehr';
+const rea = [...gh];
+console.log(...rea);
+rea[0] = 'G';
+console.log(rea);
+console.log(...gh);
+
+function sum(a,v,r) {
+    return console.log(a+v+r);
+}
+const nu = ['m','o','o'];
+sum(...nu);
+
+
+
+const num = [2, 4, 6];
+
+function sami(a, q, w) {
+    return a + q + w ;
+}
+console.log(sami(...num)); ///output 12
+
+
+function sumt() {
+    let tsum = 0;
+    for (let i = 0; i < 7; i++) {
+        if (i % 2 == 0) {
+            tsum += i;
+        }
+    }
+    return tsum;
+}
+console.log(sumt());////output 12
+
+
+function sam() {
+    const res = num.reduce((total, value) => total + value);    
+        return res
+}
+console.log( sam())//// output 12
+
+
+const nami = [];
+function namii(...nami) {
+     return nami.reduce((total,value) => total + value)
+ }
+console.log(namii(1, 2, 3, 4, 2)); //// output 12
+ 
